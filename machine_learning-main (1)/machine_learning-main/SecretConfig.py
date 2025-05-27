74f8521653552aa8ec11b25ada7790f3ba1d4443
sebas112205 <sebastianvalencia1415@gmail.com>
2025-05-27 11:28:03 -0500

    Se logro la coneccion a la base de datos

---
+++ machine_learning-main (1)/machine_learning-main/SecretConfig.py
@@ -0,0 +1,5 @@
+PGHOST='ep-cool-brook-a8o0c39i-pooler.eastus2.azure.neon.tech'
+PGDATABASE='neondb'
+PGUSER='neondb_owner'
+PGPASSWORD='npg_Zo28dBzaOVUh'
+PGPORT=5432
---
+++ machine_learning-main (1)/machine_learning-main/sql/delete_results_table.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS clustering_results
