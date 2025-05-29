3205f38b92f78d39bd2fd8e39f842cf566cfcc4f
agallego@soyudemedellin.edu.co <agallego892soyudemedellin.edu.co>
2025-05-29 00:44:30 -0500

    Ocultar

--- SecretConfig.py
+++
@@ -1,6 +0,0 @@
-# Do not expose your Neon credentials to the browser
-PGHOST='ep-dark-dew-a8gtee4y-pooler.eastus2.azure.neon.tech'
-PGDATABASE='neondb'
-PGUSER='neondb_owner'
-PGPASSWORD='npg_KSCp4nfr2LQR'
-PGPORT=5432
