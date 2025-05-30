adee31d01e4b5e06e37609e8ee1aecdb285aca05
Sebastian009 <svalencia170@soyudemedellin.edu.co>
2025-05-30 15:48:13 -0500

    SecretConfig agregado

---
+++ SecretConfig.py
@@ -0,0 +1,7 @@
+# Do not expose your Neon credentials to the browser
+
+PGHOST='ep-cool-brook-a8o0c39i-pooler.eastus2.azure.neon.tech'
+PGDATABASE='neondb'
+PGUSER='neondb_owner'
+PGPASSWORD='npg_Zo28dBzaOVUh'
+PGPORT=5432
--- machine_learning/SecretConfig.py
+++
@@ -1,5 +0,0 @@
-PGHOST='ep-cool-brook-a8o0c39i-pooler.eastus2.azure.neon.tech'
-PGDATABASE='neondb'
-PGUSER='neondb_owner'
-PGPASSWORD='npg_Zo28dBzaOVUh'
-PGPORT=5432
